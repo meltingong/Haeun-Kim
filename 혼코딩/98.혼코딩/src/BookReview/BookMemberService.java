c29e62c1c7525503dda40fd1a8c6051a64d2bf90
김하은 <116450365+meltingong@users.noreply.github.com>
2022-12-23 18:13:48 +0900

    Add files via upload

--- 혼코딩/98.혼코딩/src/BookReview/BookMemberService.java
+++ 혼코딩/98.혼코딩/src/BookReview/BookMemberService.java
@@ -7,21 +7,55 @@ public class BookMemberService {
 	
 	
 	public void idCorrect(BookMembership newMember) {
-		
-		for(int i = 0; i < members.length; i++) {
-			if(members[i] != null && members[i].getId().equals(newMember.getId()) ) {
-				System.out.println("중복된 아이디 입니다.");
-			}else if(!members[i].getId().equals(newMember.getId())) {
-				System.out.println("사용할 수 있는 아이디 입니다.");
+		char[] c1 = newMember.getId().toCharArray();
+		for(int i = 0; i < c1.length; i++) {
+			if(c1[i] >= 'a' && c1[i] <= 'z' || c1[i] >= 0 && c1[i] <=9) {
+				for(int j = 0; j < members.length; j++) {
+					if(members[j] != null && members[j].getId().equals(newMember.getId()) ) {
+						System.out.println("중복된 아이디 입니다.");
+					}else if(!members[j].getId().equals(newMember.getId())) {
+						System.out.println("사용할 수 있는 아이디 입니다.");
+				}else {
+					System.out.println("아이디는 소문자+숫자 조합으로만 가능합니다");
+				}
 			}
 		}
 		
+	}
 		
-	} //회원가입시 소문자 + 숫자형태의 아이디를 사용했는지 (어케함) 및 기존에 있는 아이디인지 검사  
+} //회원가입시 소문자 + 숫자형태의 아이디를 사용했는지 및 기존에 있는 아이디인지 검사 
 	
-	public void pwCorrect() {
-		
-		
+	private void idSlCorrect(BookMembership newMember) {
+		char[] c = newMember.getId().toCharArray();
+		for(int i = 0; i < c.length; i++) {
+			if(c[i] >= 'a' && c[i] <= 'z') {
+				
+			}
+		}		
+	}
+	
+	private void idNoCorrect(BookMembership newMember) {
+		char[] c = newMember.getId().toCharArray();
+		for(int i = 0; i < c.length; i++) {
+			if(c[i] >= 0 && c[i] <=9) {
+				
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	public void pwCorrect(BookMembership newMember) {
+		char[] c = newMember.getPw().toCharArray();
+		for(int i = 0; i < c.length; i++) {
+			if(c[i] >='A' && c[i] <= 'z' || c[i] >= 'a' && c[i] <= 'z' || c[i] >= 0 && c[i] <=9 ) {
+				
+			}
+		}
 		
 	} //회원가입시 소문자+대문자+특수문자의 조합으로 비밀번호를 설정하였는지
 	
